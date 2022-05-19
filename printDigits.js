d8c841ea3f2eba419f43cf2281a6597f26ccf0e9
Nathan Wood <nathan.ronald.wood@gmail.com>
2022-05-19 09:31:46 -0500

    Some minor adjustments

--- printDigits.js
+++ printDigits.js
@@ -1,18 +1,20 @@
 const printDigits = (num) => {
   while (num > 0) {
     console.log(num % 10);
-    num -= num % 10;
-    Math.floor((num /= 10));
+    num = Math.floor((num /= 10));
   }
 };
-
+console.log("Below should be 1");
 printDigits(1);
 // 1
+console.log("Below should be 4, 1, 3");
 printDigits(314);
 // 4
 // 1
 // 3
+console.log("Below should be 2, 1");
 printDigits(12);
 // 2
 // 1
+console.log("Below should be 6, 7, 5, 8, 9, 4, 3, 2, 5, 9, 8, 7, 6, 4, 2, 3");
 printDigits(3246789523498576);
